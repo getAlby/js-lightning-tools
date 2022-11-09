271b6ee5375caad8b1f8518fb936c8005288fb3e
Michael Bumann <hello@michaelbumann.com>
2022-11-09 09:23:32 +0100

    Export LightningAddress class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fetchWithLsat from "./lsat/fetch";
 import { boost as sendBoostagram } from "./podcasting2/boostagrams";
+import LightningAddress from "./lightning-address";
 export * as lsat from "./lsat/fetch";
 export * as boostagrams from "./podcasting2/boostagrams";
 export * as fiat from "./utils/fiat";
-export { fetchWithLsat, sendBoostagram };
+export { fetchWithLsat, sendBoostagram, LightningAddress };
