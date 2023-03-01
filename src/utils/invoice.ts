6bf8bcf1b1739673041e1fa5c300091f5d0a8a10
im-adithya <imadithyavardhan@gmail.com>
2023-03-01 14:06:00 +0530

    chore: replace bolt11 with light-bolt11-decoder

--- src/utils/invoice.ts
+++ src/utils/invoice.ts
@@ -1,18 +1,18 @@
-import { decode } from 'bolt11';
+import { decode } from 'light-bolt11-decoder';
 
-export const getHashFromInvoice = (invoice: string): string | null => {
+export const getHashFromInvoice = (invoice) => {
   if (!invoice) return null
 
   try {
     const decoded = decode(invoice)
-    if (!decoded || !decoded.tags) return null
+    if (!decoded || !decoded.sections) return null
 
-    const hashTag = decoded.tags.find(
-      (value) => value.tagName === 'payment_hash'
+    const hashTag = decoded.sections.find(
+      (value) => value.name === 'payment_hash'
     )
-    if (!hashTag || !hashTag.data) return null
+    if (!hashTag || !hashTag.value) return null
 
-    return hashTag.data.toString()
+    return hashTag.value.toString()
   } catch {
     return null
   }
