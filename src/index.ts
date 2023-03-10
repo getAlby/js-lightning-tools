77eb711f315e0589560e4fea6cd4dd6803917c4a
Michael Bumann <hello@michaelbumann.com>
2023-03-10 11:17:28 +0100

    export invoice

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import fetchWithLsat from "./lsat/fetch";
 import { boost as sendBoostagram } from "./podcasting2/boostagrams";
 import LightningAddress from "./lightning-address";
+import Invoice from "./invoice";
 export * as lsat from "./lsat/fetch";
 export * as boostagrams from "./podcasting2/boostagrams";
 export * as fiat from "./utils/fiat";
-export { fetchWithLsat, sendBoostagram, LightningAddress };
+export { fetchWithLsat, sendBoostagram, LightningAddress, Invoice };
