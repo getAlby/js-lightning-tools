c5383473ecd68add42309f39d711bd8aafcf5129
René Aaron <rene@twentyuno.net>
2023-08-09 23:32:03 +0200

    fix: rename window object

--- src/window.js
+++ src/window.js
@@ -1,3 +1,3 @@
 // assign alby-tools exports to global window object (for index.browser.js)
 // @ts-ignore
-window["albyTools"] = require('./index.cjs');
+window["lightningTools"] = require('./index.cjs');
