40b3cc4cb9af1dc49004d92213e670e902b5b25b
im-adithya <imadithyavardhan@gmail.com>
2023-10-05 10:54:00 +0530

    chore: update proxy address to api.getalby.com

--- src/lightning-address.ts
+++ src/lightning-address.ts
@@ -18,7 +18,7 @@ import { KeysendResponse } from "./types";
 const LN_ADDRESS_REGEX =
   /^((?:[^<>()\[\]\\.,;:\s@"]+(?:\.[^<>()\[\]\\.,;:\s@"]+)*)|(?:".+"))@((?:\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(?:(?:[a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const DEFAULT_PROXY = "https://lnaddressproxy.getalby.com";
+export const DEFAULT_PROXY = "https://api.getalby.com/lnurl";
 
 type LightningAddressOptions = {
   proxy?: string | false;
