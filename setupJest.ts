297dff79dde020515d80be6b5d239077e17a8326
Roland Bewick <roland.bewick@gmail.com>
2023-12-06 12:22:53 +0700

    chore: remove unnecessary crypto import in tests

--- setupJest.ts
+++ setupJest.ts
@@ -1,10 +1,3 @@
 import jestFetchMock from "jest-fetch-mock";
-import crypto from "crypto";
 
 jestFetchMock.enableMocks();
-
-Object.defineProperty(globalThis, "crypto", {
-  value: {
-    subtle: crypto.webcrypto.subtle,
-  },
-});
