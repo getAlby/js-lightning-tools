19607730e3a32f1c53314e0db8c4fd1ecf9088bf
Roland Bewick <roland.bewick@gmail.com>
2023-12-07 14:03:42 +0700

    chore: fix tests missing crypto

--- setupJest.ts
+++ setupJest.ts
@@ -1,3 +1,7 @@
+import * as crypto from "crypto";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+globalThis.crypto = crypto as any;
+
 import jestFetchMock from "jest-fetch-mock";
 
 jestFetchMock.enableMocks();
