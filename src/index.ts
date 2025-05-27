f7d2ee7b4aac49c5294263389b666be556e2f104
im-adithya <imadithyavardhan@gmail.com>
2025-05-27 16:52:03 +0530

    chore: add backwards compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./bolt11";
 export * from "./lnurl";
-export * from "./l402";
 export * from "./podcasting2";
+export * from "./l402";
 export * from "./fiat";
+export * as l402 from "./l402";
+export * as fiat from "./fiat";
