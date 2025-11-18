39026f6f4331e17ffd9deabc9976147b4a57b3e6
Dunsin <ayodejiabisuwa23@gmail.com>
2025-11-18 09:04:20 +0100

    Revert "fix: undo previous cnanges"
    This reverts commit 0651c7a84403f7e04db5518ca3d318cca16627ba.

--- src/fiat/fiat.ts
+++ src/fiat/fiat.ts
@@ -1,39 +1,5 @@
 const numSatsInBtc = 100_000_000;
 
-interface CurrencyDetails {
-  priority: number;
-  iso_code: string;
-  name: string;
-  symbol: string;
-  alternate_symbols: string[];
-  subunit: string;
-  subunit_to_unit: number;
-  symbol_first: boolean;
-  format: string;
-  html_entity: string;
-  decimal_mark: string;
-  thousands_separator: string;
-  iso_numeric: string;
-  smallest_denomination: number;
-  disambiguate_symbol?: string;
-}
-type CurrencyData = Record<string, CurrencyDetails>;
-
-export const getFiatCurrencies = async (): Promise<string[]> => {
-  const url = "https://getalby.com/api/rates";
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new Error(
-      `Failed to fetch currencies: ${response.status} ${response.statusText}`,
-    );
-  }
-
-  const data = (await response.json()) as CurrencyData;
-
-  return Object.values(data).map((currency) => currency.iso_code);
-};
-
 export const getFiatBtcRate = async (currency: string): Promise<number> => {
   const url =
     "https://getalby.com/api/rates/" + currency.toLowerCase() + ".json";
